7ada53fd44a62939b7653f17ecc99a24e30c9dd9
kwansu <kwansu91@naver.com>
2021-10-21 10:32:18 +0900

    add dynamicProgramming_02

---
+++ dynamicProgramming_02.py
@@ -0,0 +1,27 @@
+N = int(input())
+assert(2 <= N <= 30000)
+
+uncheck_list = [True] * 30000
+cur_list = [N]
+
+
+def check_and_append(n, mod):
+    if mod == 0 and uncheck_list[n]:
+        uncheck_list[n] = False
+        cur_list.append(n)
+
+
+result = -1
+n = N
+while n > 1:
+    result += 1
+    for _ in range(len(cur_list)):
+        n = cur_list.pop(0)
+        if n == 1:
+            break
+        check_and_append(n-1, 0)
+        check_and_append(*divmod(n, 5))
+        check_and_append(*divmod(n, 3))
+        check_and_append(*divmod(n, 2))
+
+print(result)
