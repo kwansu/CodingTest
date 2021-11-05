d8e74fbd109a36b7e0845409157b8a09c22d811d
kwansu <kwansu91@naver.com>
2021-11-05 09:18:47 +0900

    add graph_01

--- graph_01.py
+++ graph_01.py
@@ -1,11 +1,11 @@
 # 서로소 집합
-N, M = 5, 4
-unions = [(4, 5), (3, 4), (2, 3), (1, 2)]
+N, M = 6, 4
+unions = [(1, 4), (2, 3), (2, 4), (5, 6)]
 
 
 def find_root(n):
     if parent_info[n] != n:
-        n = find_root(parent_info[n])
+        parent_info[n] = find_root(parent_info[n])
     return parent_info[n]
 
 
@@ -13,7 +13,8 @@ parent_info = [n for n in range(N+1)]
 
 for a, b in unions:
     p, c = (a, b) if find_root(a) < find_root(b) else (b, a)
-    parent_info[c] = find_root(p)
+    temp = find_root(p)
+    parent_info[c] = temp
 
 
 print([find_root(n) for n in range(1, N+1)])
